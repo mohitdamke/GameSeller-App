45365035ea6268b0f8572b275785a29d2d6ded43
raees <mohitdamke1@gmail.com>
2024-09-07 12:11:59 +0530

    bottom bar done

--- app/src/main/java/com/example/gameseller/screen/ChatScreen.kt
+++ app/src/main/java/com/example/gameseller/screen/ChatScreen.kt
@@ -1,17 +1,20 @@
 package com.example.gameseller.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import com.example.gameseller.ui.theme.White
 
 @Composable
 fun ChatScreen(
     modifier: Modifier = Modifier,
     navController: NavHostController,
 ) {
-    Column {
+    Column(modifier = modifier.fillMaxSize().background(White)) {
         Text(text = "ChatScreen")
     }
 }
